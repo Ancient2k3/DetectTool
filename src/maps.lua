290c89b4d692cb75b7ae34a3a775066b5d12f277
DE <trinhro2022@gmail.com>
2026-01-17 21:22:47 +0700

    Refactor maps.lua to use dynamic URL construction

--- src/maps.lua
+++ src/maps.lua
@@ -1,3 +1,10 @@
-return {
-  ["IY"] = "https://raw.githubusercontent.com/EdgeIY/infiniteyield/refs/heads/master/source"
-}
+local forms = {
+  it_hub = "https://raw.githubusercontent.com/"
+} return {
+  ["IY"] = forms.it_hub.."EdgeIY/infiniteyield/refs/heads/master/source",
+  ["R-SPY"] = forms.it_hub.."Ancient2k3/RobloxScript/refs/heads/item/Remote_Spy_For_Mobile",
+  ["DEX (OLD)"] = forms.it_hub.."Ancient2k3/RobloxScript/refs/heads/item/Dex_Explorer_For_Mobile",
+  ["Custom Executor"] = forms.it_hub.."Ancient2k3/RobloxScript_0/refs/heads/main/Executor_V2",
+  ["Aimbot UI"] = forms.it_hub.."Ancient2k3/Tensura/refs/heads/main/aimbot.luau",
+  ["Animations Logger"] = forms.it_hub.."Ancient2k3/NewProject/refs/heads/main/AnimationObject.lua"
+} -- idk what i'm doing --

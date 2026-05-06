1c895424c175ae04ab9c648c7c5cb857a26a4d6a
DE <trinhro2022@gmail.com>
2026-05-06 15:49:34 +0700

    Fix Custom Executor path to include .lua extension

--- src/maps.lua
+++ src/maps.lua
@@ -4,7 +4,7 @@ local forms = {
   ["IY"] = forms.it_hub.."EdgeIY/infiniteyield/refs/heads/master/source",
   ["R-SPY"] = forms.it_hub.."Ancient2k3/RobloxScript/refs/heads/item/Remote_Spy_For_Mobile",
   ["DEX (OLD)"] = forms.it_hub.."Ancient2k3/RobloxScript/refs/heads/item/Dex_Explorer_For_Mobile",
-  ["Custom Executor"] = forms.it_hub.."Ancient2k3/RobloxScript_0/refs/heads/main/Executor_V2",
+  ["Custom Executor"] = forms.it_hub.."Ancient2k3/RobloxScript_0/refs/heads/main/Executor_V2.lua",
   ["Aimbot UI"] = forms.it_hub.."Ancient2k3/Tensura/refs/heads/main/aimbot.luau",
   ["Animations Logger"] = forms.it_hub.."Ancient2k3/NewProject/refs/heads/main/AnimationObject.lua"
 } -- idk what i'm doing --

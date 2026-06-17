4ba42a130eccb75d05f1fe2e701f49670d3ae99d
DE <trinhro2022@gmail.com>
2026-06-17 17:37:20 +0700

    Rename 'Aimbot UI' to 'Aimbot-UI' and add new entries

--- src/maps.lua
+++ src/maps.lua
@@ -5,9 +5,10 @@ local forms = {
   ["R-SPY"] = forms.it_hub.."Ancient2k3/RobloxScript/refs/heads/item/Remote_Spy_For_Mobile",
   ["DEX (OLD)"] = forms.it_hub.."Ancient2k3/RobloxScript/refs/heads/item/Dex_Explorer_For_Mobile",
   ["Custom Executor"] = forms.it_hub.."Ancient2k3/RobloxScript_0/refs/heads/main/Executor_V2.lua",
-  ["Aimbot UI"] = forms.it_hub.."Ancient2k3/Tensura/refs/heads/main/aimbot.luau",
+  ["Aimbot-UI"] = forms.it_hub.."Ancient2k3/Tensura/refs/heads/main/aimbot.luau",
   ["Animations Logger"] = forms.it_hub.."Ancient2k3/NewProject/refs/heads/main/AnimationObject.lua",
   ["Server Hop (Lowest)"] = forms.it_hub.."Ancient2k3/Tensura/refs/heads/main/server_hop_lowest_count.lua",
   ["Bay trên trời cao 😆"] = forms.it_hub.."Ancient2k3/DetectTool/refs/heads/main/fly_script.lua",
-  ["CSPY"] = forms.it_hub.."Ancient2k3/DetectTool/refs/heads/main/Cobalt_Resize_Mobile.lua"
+  ["C-SPY"] = forms.it_hub.."Ancient2k3/DetectTool/refs/heads/main/Cobalt_Resize_Mobile.lua",
+  ["UI-Builer"] = forms.it_hub.."Ancient2k3/DetectTool/refs/heads/main/UIBuilder.lua"
 } -- idk what i'm doing --
